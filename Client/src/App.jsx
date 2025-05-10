43cd6665aa5d4f289ed7dc71ca336fb110d9e459
umaruzair <umaruzair@umaruzairs-MacBook-Air.local>
2025-05-10 16:02:01 +0530

    Add: User Dashboard

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,34 +1,45 @@
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
-import RegisterPage from './Pages/RegisterPage'
-import { LoginPage } from './Pages/LoginPage'
-import { useSelector, useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import { getUser } from './Api/Auth_Api'
-import { AuthProvider, ProtectedRoute } from './Auth/AuthProvider'
-import { Welcome } from './Pages/Welcome'
-import { Error401 } from './Pages/Error401'
-import { RideBrowser } from './Pages/RideBrowser'
-import { CaptainDashboard } from './Pages/CaptainDashboard'
+"use client"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import RegisterPage from "./Pages/RegisterPage"
+import { LoginPage } from "./Pages/LoginPage"
+import { useDispatch } from "react-redux"
+import { useEffect } from "react"
+import { getUser } from "./Api/Auth_Api"
+import { AuthProvider } from "./Auth/AuthProvider"
+import { Welcome } from "./Pages/Welcome"
+import { Error401 } from "./Pages/Error401"
+import { RideBrowser } from "./Pages/RideBrowser"
+import { CaptainDashboard } from "./Pages/CaptainDashboard"
+import { UserDashboard } from "./Pages/UserDashboard"
 
 function App() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   useEffect(() => {
-    getUser(dispatch);
+    getUser(dispatch)
   }, [dispatch])
-  
+
   return (
     <>
-      <BrowserRouter> 
+      <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<LoginPage/>}/>
-          <Route path='/register' element={<RegisterPage/>}/>
-          <Route path='/401' element={<Error401/>}/>
-          <Route path='/welcome' element={<Welcome/>}/>
-          <Route path='/' element={<AuthProvider>
-            <RideBrowser/>
-          </AuthProvider>}/>
-          {/* <Route path='/captain' element={<ProtectedRoute><CaptainDashboard/></ProtectedRoute>}/> */}
-          <Route path='/captain' element={<CaptainDashboard/>}/>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/401" element={<Error401 />} />
+          <Route path="/welcome" element={<Welcome />} />
+          <Route
+            path="/"
+            element={
+                <RideBrowser />
+            }
+          />
+          <Route path="/captain" element={<CaptainDashboard />} />
+          <Route
+            path="/dashboard"
+            element={
+                <UserDashboard />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
