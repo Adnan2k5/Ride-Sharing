c70b7bec4255ccca89c93d66188b2029ed16a34d
Adnan Ashraf <ashrafadnan959@gmail.com>
2025-08-13 09:49:31 +0530

    fixed: app.jsx basename

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename='/ride-sharing'>
         <Toaster />
         <Routes>
           <Route path='/login' element={<LoginPage />} />
